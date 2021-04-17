89faf4f3875ec2e8283cc541fcd7940f6392c504
Carlo Enrico <carlo.enrico@uol.com.br>
2021-04-17 20:45:44 -0300

    chore: UP Challenge 6

--- Module-2/Challenge-6/src/middlewares/upload.ts
+++ Module-2/Challenge-6/src/middlewares/upload.ts
@@ -1,16 +1,18 @@
 import { Request, Response, NextFunction } from 'express';
 import storage from '../config/storage';
-import AppError from '../errors/AppError';
 
-export default (req: Request, res: Response, next: NextFunction): void => {
-  const upload = storage().single('filedata');
+export default async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> => {
+  const upload = storage().single('file');
 
   upload(req, res, (err: Error | string): Response | void => {
     if (err) {
       const error = err as Error;
-      throw new AppError(error.message, 500);
+      return res.status(500).json({ status: 'error', message: error.message });
     }
-
-    next(null);
+    return next();
   });
 };
--- Module-2/Challenge-6/src/routes/transactions.routes.ts
+++ Module-2/Challenge-6/src/routes/transactions.routes.ts
@@ -10,25 +10,4 @@ transactionsRouter.post('/', transactionController.store);
 transactionsRouter.delete('/:id', transactionController.destroy);
 transactionsRouter.post('/import', upload, importController.store);
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
-// import DeleteTransactionService from '../services/DeleteTransactionService';
-// import ImportTransactionsService from '../services/ImportTransactionsService';
-
-// transactionsRouter.get('/', async (request, response) => {
-//   // TODO
-// });
-
-// transactionsRouter.post('/', async (request, response) => {
-//   // TODO
-// });
-
-// transactionsRouter.delete('/:id', async (request, response) => {
-//   // TODO
-// });
-
-// transactionsRouter.post('/import', async (request, response) => {
-//   // TODO
-// });
-
 export default transactionsRouter;

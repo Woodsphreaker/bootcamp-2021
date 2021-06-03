c00a4ece4aee0e31958e025a61f13ae90a2899b4
Carlo Enrico <carlo.enrico@uol.com.br>
2021-06-03 11:40:39 -0300

    chore: update project

--- Module-3/create-react-app-with-ts/src/pages/Dashboard/index.tsx
+++ Module-3/create-react-app-with-ts/src/pages/Dashboard/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect, ChangeEvent, FormEvent } from 'react';
+import api from '../../services/api';
 
 import {
   LogoGit,
@@ -11,17 +12,38 @@ import {
 } from './styles';
 
 const Dashboard: React.FC = () => {
+  const [repositories, setRepositories] = useState([]);
+  const [repoName, setRepoName] = useState('');
+
+  const handleRepoNameChange = ({
+    target: { value },
+  }: ChangeEvent<HTMLInputElement>): void => {
+    setRepoName(value);
+  };
+
+  const handleSubmit = (event: FormEvent): void => {
+    event.preventDefault();
+    findRepoByName();
+  };
+
+  const findRepoByName = async (): Promise<void> => {
+    const { data: repository } = await api.get(`repos/${repoName}`);
+    console.log(repository);
+  };
+
   return (
     <>
       <LogoGit />
       <Title>Explore repositórios no Github.</Title>
 
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <input
           type="text"
           name=""
           id=""
+          value={repoName}
           placeholder="Digite no nome do repositório"
+          onChange={handleRepoNameChange}
         />
         <button type="submit">Pesquisar</button>
       </Form>
